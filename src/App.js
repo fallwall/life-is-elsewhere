4a9fd56ee4524fc2148097609a23f9b8e09ec22b
XX <fallwall19@gmail.com>
2020-05-31 12:38:28 -0700

    main page set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TopNav from './components/TopNav';
+import Nav from './components/Nav';
+import Main from './pages/Main';
+import RightNav from './components/RightNav';
+import Footer from './components/Footer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isMain: true
+    }
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <TopNav />
+        <Nav />
+        {this.state.isMain && <Main />}
+        <RightNav />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className="page-main">
+      never fear, main is here.
+    </div>
+  )
+}
+
+export default Main;
