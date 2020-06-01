951670826c044f6bdb34e979bff87f2d1811c9e7
XX <fallwall19@gmail.com>
2020-06-01 11:18:49 -0700

    right nav hamburg on click

--- src/components/RightNav.jsx
+++ src/components/RightNav.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './RightNav.css';
 
 const RightNav = () => {
+  const [burger, setBurger] = useState(false);
   return (
-    <div>
+    <div className="rightnav">
+      <button
+        onClick={()=>setBurger(!burger)}
+        className={burger ? "hamburger hamburger--collapse is-active" : "hamburger hamburger--collapse"}
+        type="button">
+        <span className="hamburger-box">
+          <span className="hamburger-inner"></span>
+        </span>
+      </button>
+      {/* right side nav bar */}
 
     </div>
   )
