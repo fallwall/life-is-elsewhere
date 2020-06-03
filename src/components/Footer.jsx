fad6a6a0371c5121ad50ae831ac265695eabcf8e
XX <fallwall19@gmail.com>
2020-06-03 10:26:00 -0700

    footer added. content to be replaced

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const Footer=()=> {
+const Footer = () => {
   return (
-    <div>
-      
+    <div className="footer">
+      <div className="footer-sec-1">
+        <h2>Location</h2>
+        <p>Vancouver, BC</p>
+      </div>
+      <div className="footer-sec-2">
+        <h2>Contact</h2>
+        <p>info@tibbyelsewhere.com</p>
+      </div>
+      <div className="footer-sec-3">
+        <h2>Socials</h2>
+        <p>Instagram</p>
+      </div>
+      <div className="footer-sec-4">
+        <h2>About</h2>
+        <p>Tibby Xu</p>
+      </div>
     </div>
   )
 }
