fa7c92bf0d7531ded9b753c31d92a1394a89b7e3
XX <fallwall19@gmail.com>
2020-06-02 22:10:59 -0700

    post page css bits and pieces

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getOnePost } from '../services/wordpress';
+import './Post.css';
 
 class Post extends Component {
   constructor(props) {
@@ -12,18 +13,19 @@ class Post extends Component {
   componentDidMount = async () => {
     const resp = await getOnePost(this.props.postID);
     const currentPost = resp["data"];
-
     this.setState({
       currentPost
     })
+    console.log(this.state.currentPost);
   }
 
   render() {
     return (
       <div className="page-post">
+        <div className="page-post-title"><h1>{this.state.currentPost.title}</h1></div>
         <div className="page-post-content"
-        dangerouslySetInnerHTML={{ __html: `${this.state.currentPost.content}` }}
-      ></div>
+          dangerouslySetInnerHTML={{ __html: `${this.state.currentPost.content}` }}
+        ></div>
       </div>
     )
   }
