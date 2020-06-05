fc73063f2824d77ae3820961c62af93ff32b65df
XX <fallwall19@gmail.com>
2020-06-04 20:53:52 -0700

    change thumb to square

--- src/components/PostExcerpt.jsx
+++ src/components/PostExcerpt.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './PostExcerpt.css';
 
+
+
+
 const PostExcerpt = ({ children }) => {
   return (
-    <div className="wordpress-excerpt-item">
+    <div
+      className="wordpress-excerpt-item">
       <div className="wordpress-excerpt-item-title"><h1>{children.title}</h1></div>
-      <div className="wordpress-excerpt-item-img"><img src={children.featured_image} /></div>
+      <div style={{   background: `url(${children.featured_image}) 50% 50% no-repeat`, 
+      width: `250px`,
+      height: `250px` }}></div>
+      {/* <div className="wordpress-excerpt-item-img"><img src={children.featured_image} /></div> */}
       <div className={`wordpress-excerpt-item-excerpt excerpt-${children.ID}`}
         dangerouslySetInnerHTML={{ __html: `${children.excerpt}` }}
       ></div>
