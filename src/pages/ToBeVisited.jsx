8f4c20982a68355c5191ee5b58944d37695ffd90
XX <fallwall19@gmail.com>
2020-06-09 12:55:41 -0700

    tobevisited connected

--- src/pages/ToBeVisited.jsx
+++ src/pages/ToBeVisited.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Grid from '../components/Grid';
+import Tile from '../components/Tile';
+import data from '../data/cities.json';
+
+const brakePoints = [500, 840, 1260];
 
 const ToBeVisited=()=> {
   return (
-    <div>
-      to be
+    <div className="visited-container">
+      <div className="grid-container">
+        <Grid brakePoints={brakePoints}>
+          {data.tobevisited.map(c => <Tile key={c.id} data={c} />)}
+        </Grid>
+      </div>
     </div>
   )
 }
